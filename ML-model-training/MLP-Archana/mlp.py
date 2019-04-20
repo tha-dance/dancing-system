7894938e6361ec66aafcc24a81a1c0270c6ce126
archthegit <archieprad@gmail.com>
2019-04-20 12:06:23 +0800

    added my part of the ml stuff

---
+++ ML-model-training/MLP-Archana/mlp.py
@@ -0,0 +1,64 @@
+import pandas as pd
+import numpy as np
+from pandas import read_csv
+from sklearn import preprocessing
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split 
+from sklearn.preprocessing import StandardScaler  
+from sklearn.neural_network import MLPClassifier
+from sklearn.model_selection import StratifiedKFold
+from sklearn.metrics import classification_report, confusion_matrix 
+from sklearn.model_selection import cross_val_score 
+from sklearn.externals import joblib
+
+
+
+def load_file(filepath):
+	data_frame = read_csv(filepath, header=None, delim_whitespace=True)
+	return data_frame.values
+
+def load_dataset_group(group,prefix = ''):
+	X = load_file(prefix+group+'/trainx_17april_logout'+'.txt')
+	y = load_file(prefix+group+'/trainy_17april_logout'+'.txt')
+	print(np.where(np.isnan(X)))
+	return X,y
+
+
+def load_dataset(prefix=''):
+	X,Y = load_dataset_group('train',prefix+'./Datasets_new/')
+
+	trainX, testX, trainy, testy = train_test_split(X, Y, test_size=0.05, random_state = 2)
+	trainy, testy = trainy[:,0], testy[:,0]
+	return trainX, trainy, testX, testy
+
+def evaluate_model (testX,testy,model):
+	pred = model.predict(testX);
+	accuracy = accuracy_score(pred,testy);
+	print (" Confusion matrix \n", confusion_matrix(testy, pred))
+
+	return accuracy*100.0
+
+
+trainX, trainy, testX, testy = load_dataset()
+
+scaler = StandardScaler()
+scaler.fit(trainX)
+
+trainX = scaler.transform(trainX)
+testX = scaler.transform(testX)
+
+kf = StratifiedKFold(n_splits=5,random_state=4)
+mlp = MLPClassifier(hidden_layer_sizes=(40,40,40))
+mlp.fit(trainX,trainy);
+
+joblib.dump(mlp,"mlp_19april_logout")
+joblib.dump(scaler, 'scaler_19april_logout.joblib')
+
+model = joblib.load("mlp_19april_logout")
+
+results = evaluate_model (testX,testy,model)
+trial = cross_val_score(model,trainX,trainy,cv=kf)
+
+print('results = %.3f' %results,'cross_val_score = %.3f' %(trial.mean()*100));
+
+
---
+++ ML-model-training/RF-Archana/randomforest.py
@@ -0,0 +1,78 @@
+
+from pandas import read_csv
+from sklearn.metrics import accuracy_score
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.feature_selection import SelectFromModel
+from sklearn.metrics import confusion_matrix
+from sklearn.model_selection import StratifiedKFold
+from sklearn.model_selection import train_test_split
+from sklearn.model_selection import cross_val_score
+from sklearn.externals import joblib
+import numpy as np
+from matplotlib import pyplot as plt
+
+def load_file(filepath):
+	data_frame = read_csv(filepath, header=None, delim_whitespace =True)
+	return data_frame.values
+
+def load_features():
+	features = load_file('./HARDataset/features.txt')
+	return features
+
+def load_dataset_test(filename):
+	X = load_file(filename)
+	return X;
+
+def load_dataset_group(group,prefix = ''):
+	X = load_file(prefix+group+'/trainx_2models'+'.txt')
+	y = load_file(prefix+group+'/trainy_2models'+'.txt')
+	return X,y
+
+def load_dataset(prefix=''):
+	X,Y = load_dataset_group('train',prefix+'./Datasets_new/')
+
+	trainX, testX, trainy, testy = train_test_split(X, Y, test_size=0.4, random_state = 2)
+	#print(testX.shape, testy.shape)
+
+	trainy, testy = trainy[:,0], testy[:,0]
+	return trainX, trainy, testX, testy
+	#return X,Y
+def evaluate_model (testX,testy,model):
+	pred = model.predict(testX);
+	accuracy = accuracy_score(pred,testy);
+	print (" Confusion matrix \n", confusion_matrix(testy, pred))
+
+	return accuracy*100.0
+
+
+
+trainX,trainy,testX,testy= load_dataset()
+#features = load_features()
+#model = RandomForestClassifier(n_estimators =95,criterion = "entropy",max_features = "log2")
+kf = StratifiedKFold(n_splits=5,random_state=4)
+rf = RandomForestClassifier(n_estimators = 200,criterion = "gini", n_jobs= -1);
+sfm = SelectFromModel(rf,threshold = 0.00001)
+
+#testX = load_dataset_test('./raffles_testx.txt')
+#testy = load_dataset_test('./raffles_testy.txt')
+
+sfm.fit(trainX,trainy)
+trainX_imp = sfm.transform(trainX);
+testX_imp = sfm.transform(testX);
+rf.fit(trainX_imp,trainy)
+joblib.dump(rf,"rf_final")
+model = joblib.load("rf_final")
+results = evaluate_model(testX_imp,testy,model)
+trial = cross_val_score(model,trainX,trainy,cv=kf)
+#for name, importance in zip(features, model.feature_importances_):
+#	print(name, "=", importance)
+
+ #plt.bar(range(len(model.feature_importances_)), model.feature_importances_)
+ #plt.show()
+
+#print("prarameters in use: ",model.get_params());
+print('results = %.3f' %results,'cross_val_score = %.3f' %(trial.mean()*100));
+
+
+
+
